e5c6a5a0b884f8d5a2252fdf6a1526ddd5ed1ac1
chazzybearr <leoncai@outlook.com>
2023-03-22 02:56:13 -0400

    create Echo.java

---
+++ src/main/java/commands/Echo.java
@@ -0,0 +1,28 @@
+package commands;
+
+import state.ShellState;
+
+import java.util.List;
+
+public class Echo extends Command{
+    static int MIN_ARGUMENTS = 0;
+    static int MAX_ARGUMENTS = 100;
+
+    public Echo() {
+        super(MIN_ARGUMENTS, MAX_ARGUMENTS);
+    }
+
+    @Override
+    public String runCommand(ShellState state, List<String> arguments) throws Exception {
+
+        StringBuilder returnString = new StringBuilder();
+        for (String word : arguments) {
+            returnString.append(word).append(" ");
+        }
+        if (returnString.isEmpty()) {
+            return "\n";
+        }
+        return returnString.deleteCharAt(returnString.length() - 1).append("\n").toString();
+    }
+
+}
